f7362acda7f6f005214c8e21abbd51b5cc0e0e5c
Michael Litsos <34893575+MichaelDee98@users.noreply.github.com>
2023-04-17 15:44:48 +0300

    added comment

--- src/main/java/gr/athtech/toDoAppWithLogin/ToDoAppWithLoginApplication.java
+++ src/main/java/gr/athtech/toDoAppWithLogin/ToDoAppWithLoginApplication.java
@@ -1,5 +1,5 @@
 package gr.athtech.toDoAppWithLogin;
-
+// This is a comment
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
